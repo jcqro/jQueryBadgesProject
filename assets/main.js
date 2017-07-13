708f565b3e1b3b87a3396df3cd9ed14a7996a900
jcqro <jc.ferher@gmail.com>
2017-07-13 12:14:25 -0500

    Projecto Completado

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,34 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/jcqro.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      addCourses(response.courses.completed);
+    }
+  });
+  function addCourses(courses){
+    var $badges = $('#badges');
+    courses.forEach(function (course){
+      
+        var $course = $('<div />',{
+          class: 'course',
+        }).appendTo($badges);
 
+        $('<h3 />', {
+          text: course.title,
+        }).appendTo($course);
+        $('<img />',{
+          src:course.badge
+        }).appendTo($course);
+        $('<a />',{
+          href:course.url,
+          target: '_blank',
+          'class':'btn btn-primary',
+          text: 'See Course'
+        }).appendTo($course);
+    })
+  }
 });
